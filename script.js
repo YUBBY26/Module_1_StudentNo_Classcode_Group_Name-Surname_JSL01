e6257dc52c9e933e7aa1d981fe06e78298025d45
YUBBY26 <heyubar26@gmail.com>
2024-08-23 22:10:32 +0200

    JSL01

--- script.js
+++ script.js
@@ -2,9 +2,15 @@ function validateSyntax() {
     let input = document.getElementById('petInput').value;
     // Validation logic goes here
     let result = ''; // Placeholder for validation result
-
+    let reg = /^pet_[a-zA-Z0-9]+$/;
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
+    if (reg.test(input)) {
+        result += 'Valid Syntax.';
+    } else {
+        result += 'Invalid Syntax.';
+    }
+    // Display the validation result
 
             document.getElementById('result').innerText = result;
 }
